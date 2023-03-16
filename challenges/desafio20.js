3b129715873af7cd0f2a8d5d7da982397065ad61
Raphael Pacheco <raphaelrogen@gmail.com>
2023-03-16 10:27:13 -0300

    requisito 22

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,4 +1,4 @@
-db.produtos.updateOne({ nome: "Quarteirão com Queijo" },
+db.produtos.updateOne( { nome: "Quarteirão com Queijo" },
  { $pop: { ingredientes: -1 } }
  );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
